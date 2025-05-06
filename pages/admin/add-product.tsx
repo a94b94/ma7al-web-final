a8ef2734778a57e4d95d23fc714264a7734f973f
a94b94 <abd5iqqq@gmail.com>
2025-05-06 19:30:06 +0300

    fix: remove invalid call to stopDecoding in barcode scanner

--- pages/admin/add-product.tsx
+++ pages/admin/add-product.tsx
@@ -41,8 +41,6 @@ export default function AddProductPage() {
           setBarcode(result.getText());
           checkBarcode(result.getText());
           setScanning(false);
-          codeReader.stopDecoding(); // 
-
         }
       });
     }
