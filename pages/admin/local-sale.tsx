918fc405868e528ad95de703ee9a410f6f17bdd1
a94b94 <abd5iqqq@gmail.com>
2025-05-09 20:32:10 +0300

    حذف storeLogo من صفحة local-sale.tsx

--- pages/admin/local-sale.tsx
+++ pages/admin/local-sale.tsx
@@ -139,7 +139,6 @@ export default function LocalSalePage() {
           <InvoicePreview
             order={fakeOrder}
             storeName={storeName}
-            storeLogo={storeLogo}
             showActions={true}
           />
         </div>
